34016805a29b95716f22ba120bbb08be0bab63c6
Metehan Çavdar <metehan262626@gmail.com>
2021-03-02 18:24:29 +0300

    MetoFİnalProject

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,7 @@
 ﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Result;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.İnMemory;
 using Entities.Concrete;
@@ -17,30 +20,43 @@ namespace Business.Concrete
         {
             _productDal = productDal;
         }
-
-        public List<Product> GetAllByCategoryId()
+        public IResult Add(Product product)
         {
-            //İş kodları
-            //Yetkisi var mı?
-            
-            return _productDal.GetAll();
-            
+            //business codes
+            if (product.ProductName.Length < 2)
+            {
+                //magic strings
+                return new ErrorResult(Messages.ProductNameInvalid);
+            }
+            _productDal.Add(product);
+            return new SuccessResult(Messages.ProductAdded);
+        }
+        public IDataResult<List<Product>> GetAll()
+        {
+            if (DateTime.Now.Hour == 22)
+            {
+                return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
+            }
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(), Messages.ProductListed);
         }
 
-        public List<Product> GetByUNitPrice(int id)
+        public IDataResult<List<Product>> GetAllByCategoryId(int id)
         {
-            //CategoryId göndeilen id'ye eşitse onları filtrele
-            return _productDal.GetAll(p => p.CategoryId == id);
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p=>p.CategoryId==id));
         }
 
-        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        public IDataResult<Product> GetById(int productId)
         {
-            return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
+            return new SuccessDataResult<Product>(_productDal.Get(p=>p.ProductId==productId));
         }
 
-        public List<ProductDetailDto> GetProductDetails()
+        public IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max)
         {
-            return _productDal.GetProductDetails();
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max));
+        }
+        public IDataResult<List<ProductDetailDto>> GetProductDetails()
+        {
+            return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetails());
         }
     }
 }
---
+++ Core/Utilities/Results/SuccessDataResult.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class SuccessDataResult<T>:DataResult<T>
+    {
+        
+        // Data ve mesaj ver
+        public SuccessDataResult(T data, string message):base(data,true,message)
+        {
+
+        }
+         //Sadece data ver
+         public SuccessDataResult(T data) :base(data,true)
+        {
+
+        }
+        //Sadece mesaj ver
+        public SuccessDataResult(string message):base(default,true,message)
+        {
+
+        }
+        //Hiçbir şey verme
+        public SuccessDataResult() :base(default,true)
+        {
+                
+        }
+
+       }
+    }
+
